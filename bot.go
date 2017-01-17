06b1145e17f3563fca00d34ccd19cb2541c91cd6
Maxim Lebedev <mail@toby3d.ru>
2017-01-17 22:21:10 +0500

    🗣 Echo-bot
    Django Lumiere - Safety Behaviours

---
+++ bot.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"log"
+
+	tg "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+const token = ""
+
+var bot *tg.BotAPI
+
+func init() {
+	log.Println("[INIT] I'm alive!")
+
+	var err error
+	bot, err = tg.NewBotAPI(token)
+	if err != nil {
+		log.Fatalf("[BOT] Initialize error: %+v", err)
+	}
+	log.Printf("[BOT] Authorized as @%s", bot.Self.UserName)
+	bot.Debug = true
+}
+
+func main() {
+	updates := make(<-chan tg.Update)
+
+	upd := tg.NewUpdate(0)
+	upd.Timeout = 60
+	updates, err := bot.GetUpdatesChan(upd)
+	if err != nil {
+		log.Printf("[ERROR][UPDATES] %s", err.Error())
+	}
+
+	// Updater
+	for update := range updates {
+		if update.Message != nil {
+			go sendEcho(update.Message)
+		}
+	}
+}
+
+func sendEcho(msg *tg.Message) {
+	log.Printf("[LOG] %s: %s", msg.From.UserName, msg.Text)
+
+	echo := tg.NewMessage(msg.Chat.ID, msg.Text)
+	echo.ReplyToMessageID = msg.MessageID
+
+	if _, err := bot.Send(echo); err != nil {
+		log.Printf("[ERROR][SEND] %s", err.Error())
+	}
+}
